43ca574c1acbbc83cbdf53c4fe8e140517ae7d5b
Nikosane <linux.nikosane@gmail.com>
2025-05-01 23:16:27 +0530

    function updated

--- utils/visualization.py
+++ utils/visualization.py
@@ -10,3 +10,12 @@ def plot_trajectories(X, y_true, y_pred=None, title="Time Series Trajectories"):
     ax = fig.add_subplot(121, projection='3d')
     ax.plot(y_true[0, :, 0], y_true[0, :, 1], y_true[0, :, 2], label="True")
     ax.set_title("True Trajectory")
+    
+    if y_pred is not None:
+        ax2 = fig.add_subplot(122, projection='3d')
+        ax2.plot(y_pred[0, :, 0], y_pred[0, :, 1], y_pred[0, :, 2], label="Predicted", color='r')
+        ax2.set_title("Predicted Trajectory")
+
+    plt.tight_layout()
+    plt.show()
+
